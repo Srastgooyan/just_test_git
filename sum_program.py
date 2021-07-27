57d4c7fae60b74d339bfc3d739beb7c9607c05fd
s.rastgooyan <s.rastgooyan0828@gmail.com>
2021-07-27 14:03:23 -0700

    calculate sum of two numbers

---
+++ sum_program.py
@@ -0,0 +1,5 @@
+get_the_first_number_from_user = int(input("Please enter the firs number : "))
+get_the_second_number_from_user = int(input("Please enter the second number : "))
+
+print("The result of sum of two number that you entered is : {get_the_first_number_from_user  + get_the_second_number_from_user}")
+
